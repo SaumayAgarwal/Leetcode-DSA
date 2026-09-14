833d695b724394a3f8a98b4c49c753e56ad9a596
Saumay Agrawal <saumayagarwal6397@gmail.com>
2026-09-14 22:24:56 +0530

    Time: 0 ms (100%), Space: 46.5 MB (73.5%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int[] dfs(TreeNode root){
+        if(root==null)return new int[]{0, 0, 0};
+
+        int[] left=dfs(root.left);
+        int[]  right=dfs(root.right);
+
+        int val=0;
+
+        if(root.val+left[1]+left[2]+right[1]+right[2]>right[0]+left[0]){
+            val=root.val+left[1]+left[2]+right[1]+right[2];
+        }
+        else{
+            val=right[0]+left[0];
+        }
+
+        return new int[]{val, left[0], right[0]};
+    }
+    public int rob(TreeNode root) {
+        return dfs(root)[0];
+    }
+}
